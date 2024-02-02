c93bae6f4047a3a3b1de85e752361d55dfb005c8
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-02 15:45:42 +0000

    Back end and front end connected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import councillorList from './councillors';
+import hallsList from './halls';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
@@ -11,6 +12,45 @@ function App() {
   let pronounA = "I"
   let hall = "Uphall"
   let name = "John Smith"
+  let letter0 = {
+    content: [
+      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
+      ' ',
+      {text:'Uphall Station Village Hall', alignment:"right" },
+      {text:'14 Marrfield Terrace',alignment:"right" },
+      {text:'Uphall Station',alignment:"right" },
+      {text:'EH54 5PY',alignment:"right" },
+      {text:'West Lothian',alignment:"right" },
+      {text:'United Kingdom',alignment:"right" },
+      ' ',
+      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
+      {text:'Contact Number: ',alignment:"right" },
+      {text:`Dear Councillor ${councName},`},
+      ' ',
+      `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
+      `,
+      ' ',
+      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
+      ' ',
+      ' ',
+      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
+      ' ',
+      {
+        ul: [
+          'What progress has been made in developing budget cut plans',
+          'Which Community Centres are at risk of closure',
+          'What information has the Council used to target budget cuts',
+          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
+        ]
+      },
+      ' ',
+      'Yours Sincerely,',
+      ' ',
+      `${name}`,
+  
+    ]
+    
+  }
   let letter1 = {
     content: [
       {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
@@ -50,35 +90,132 @@ function App() {
     ]
     
   }
+  let letter2 = {
+    content: [
+      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
+      ' ',
+      {text:'Uphall Station Village Hall', alignment:"right" },
+      {text:'14 Marrfield Terrace',alignment:"right" },
+      {text:'Uphall Station',alignment:"right" },
+      {text:'EH54 5PY',alignment:"right" },
+      {text:'West Lothian',alignment:"right" },
+      {text:'United Kingdom',alignment:"right" },
+      ' ',
+      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
+      {text:'Contact Number: ',alignment:"right" },
+      {text:`Dear Councillor ${councName},`},
+      ' ',
+      `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
+      `,
+      ' ',
+      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
+      ' ',
+      ' ',
+      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
+      ' ',
+      {
+        ul: [
+          'What progress has been made in developing budget cut plans',
+          'Which Community Centres are at risk of closure',
+          'What information has the Council used to target budget cuts',
+          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
+        ]
+      },
+      ' ',
+      'Yours Sincerely,',
+      ' ',
+      `${name}`,
+  
+    ]
+    
+  }
+  let letter3 = {
+    content: [
+      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
+      ' ',
+      {text:'Uphall Station Village Hall', alignment:"right" },
+      {text:'14 Marrfield Terrace',alignment:"right" },
+      {text:'Uphall Station',alignment:"right" },
+      {text:'EH54 5PY',alignment:"right" },
+      {text:'West Lothian',alignment:"right" },
+      {text:'United Kingdom',alignment:"right" },
+      ' ',
+      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
+      {text:'Contact Number: ',alignment:"right" },
+      {text:`Dear Councillor ${councName},`},
+      ' ',
+      `As you will no doubt be aware our community centre in ${hall[0]} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
+      `,
+      ' ',
+      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
+      ' ',
+      ' ',
+      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
+      ' ',
+      {
+        ul: [
+          'What progress has been made in developing budget cut plans',
+          'Which Community Centres are at risk of closure',
+          'What information has the Council used to target budget cuts',
+          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
+        ]
+      },
+      ' ',
+      'Yours Sincerely,',
+      ' ',
+      `${name}`,
+  
+    ]
+    
+  }
+
+  let letters = [letter0, letter1, letter2, letter3]
   
   function createLetter(){
+    let feed = document.getElementById("feedback")
     let tempHall = document.getElementById("hall");
-    hall = tempHall.value
-    if (hall === "null") return
+    if (tempHall.value === "null") return feed.innerHTML = "Please select a hall"
+    else hall = hallsList[tempHall.value]
     let tempCounc = document.getElementById("counc")
-    if (tempCounc.value === null) return
+    console.log(tempCounc.value)
+    if (tempCounc.value === "null") return feed.innerHTML = "Please select a ward"
     else tempCounc = councillorList[tempCounc.value]
     console.log(tempCounc)
-    // council("Boyle")
-    councilTwo(letter1,tempCounc[0])
-   
+
+    for (let i=0; i< tempCounc.length; i++){
+      council(letters[i],tempCounc[i], tempHall.text)
+    }
+
+    feed.innerHTML = `Letters to Councillors have been written and are ready to print`
     
-    // await council("Cartmill")
-    
-    // councName = "Pearson"
-    
-    // councName = "Stafford"
-    
+      
     return
   }
 
 
-  function councilTwo(doc,nameCounc){
+  function council(doc,nameCounc, hallName){
     councName = nameCounc
+    for (let i=0; i<6; i++) doc.content[i+2] = hall[i]
+    let telNo = document.getElementById("tel")
+    if (telNo.value.length > 1) doc.content[10] = {text:`Contact Number: ${telNo.value}`,alignment:"right" }
+    else doc.content[10] = ''
     doc.content[11] = {text:`Dear Councillor ${councName},`}
-    doc.content[13] = `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
+    doc.content[13] = `As you will no doubt be aware our community centre in ${hallName} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
     `
-    pdfMake.createPdf(letter1).download()
+    let sigName = document.getElementById("sig")
+    if (sigName.value.length > 0) doc.content[24] = sigName.value
+    else doc.content[24] = ''
+
+    if (document.getElementById("group").checked){
+      doc.content[15] = `We are writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`
+      doc.content[18] = `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees we would like to ask the following questions:`
+    }
+    else{
+      doc.content[15] = `I am writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`
+      doc.content[18] = `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees I would like to ask the following questions:`
+    }
+    
+    pdfMake.createPdf(doc).download()
   }
 
   
@@ -91,9 +228,38 @@ function App() {
         <label>Community Centre : </label>
         <select name="hall" id="hall">
           <option value="null">select hall</option>
-          <option value="Uphall Station Village Hall">Uphall Station Village Hall</option>
-          <option value="Strathbrock Community Centre">Strathbrock Community Centre</option>
-
+          <option value="Addiewell">Addiewell Community Centre</option>
+          <option value="Bellsquarry">Bellsquarry Village Hall</option>
+          <option value="Boghall">Boghall Community Wing</option>
+          <option value="Bridgend">Bridgend Community Centre</option>
+          <option value="Broxburn">Broxburn Old Town Centre</option>
+          <option value="Carmondean">Carmondean Community Centre</option>
+          <option value="Chalmers">Chalmers Hall (Linlithgow Bridge)</option>
+          <option value="Craiginn">Craiginn Community Centre</option>
+          <option value="Crofthead">Crofthead Community Centre</option>
+          <option value="Dechmont">Dechmont Hall</option>
+          <option value="EastWhitburn">East Whitburn Community Centre</option>
+          <option value="Ecclesmachan">Ecclesmachan Village Hall</option>
+          <option value="Forestbank">Forestbank Community Centre</option>
+          <option value="Kirknewton">Kirknewton Village Hall</option>
+          <option value="Lanthorn">Lanthorn Community Centre</option>
+          <option value="Livingston">Livingston Station Community Centre</option>
+          <option value="MidCalder">Mid Calder Community Centre</option>
+          <option value="Murieston">Murieston Village Hall</option>
+          <option value="Mosswood">Mosswood Community Centre</option>
+          <option value="Newton">Newton Community Centre</option>
+          <option value="Newyearfield">Newyearfield Farm Community Centre</option>
+          <option value="Polbeth">Polbeth HUB</option>
+          <option value="Philpstoun">Philpstoun Community Centre</option>
+          <option value="Seafield">Seafield Community Centre</option>
+          <option value="Stoneyburn">Stoneyburn Community Centre</option>
+          <option value="Strathbrock">Strathbrock Community Centre</option>
+          <option value="Torphichen">Torphichen Community Centre</option>
+          <option value="UphallStation">Uphall Station Village Hall</option>
+          <option value="UphallCom">Uphall Community Centre</option>
+          <option value="Winchburgh">Winchburgh Community Centre</option>
+          <option value="WestCalder">West Calder Community Centre</option>
+          <option value="Whitburn">Whitburn Community Centre</option>
       </select>
       </div>
 
@@ -126,9 +292,17 @@ function App() {
         <label>Signature (optional)</label>
         <input type="text" id="sig"></input>
       </div>
-
+      <div>
+        <label>Contact Number (optional)</label>
+        <input type="tel" id="tel"></input>
+      </div>
+    <br/>
       <div>
       <button onClick={createLetter}>Create Letters to Councillors</button>
+      </div>
+      <br/>
+      <div>
+        <label id="feedback"></label>
       </div>
       
     </div>
