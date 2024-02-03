4869fdc6e3997159c5b7733e7dd6aef2254d3edc
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-02-03 12:52:14 +0000

    Cleaned up variables and moved code to components

--- src/App.js
+++ src/App.js
@@ -3,173 +3,14 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import councillorList from './councillors';
 import hallsList from './halls';
+import letters from './letters';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
 function App() {
-  let councName = "Boyle"
-  let pronoun = "I am"
-  let pronounA = "I"
-  let hall = "Uphall"
-  let name = "John Smith"
-  let letter0 = {
-    content: [
-      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
-      ' ',
-      {text:'Uphall Station Village Hall', alignment:"right" },
-      {text:'14 Marrfield Terrace',alignment:"right" },
-      {text:'Uphall Station',alignment:"right" },
-      {text:'EH54 5PY',alignment:"right" },
-      {text:'West Lothian',alignment:"right" },
-      {text:'United Kingdom',alignment:"right" },
-      ' ',
-      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
-      {text:'Contact Number: ',alignment:"right" },
-      {text:`Dear Councillor ${councName},`},
-      ' ',
-      `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
-      `,
-      ' ',
-      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
-      ' ',
-      ' ',
-      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
-      ' ',
-      {
-        ul: [
-          'What progress has been made in developing budget cut plans',
-          'Which Community Centres are at risk of closure',
-          'What information has the Council used to target budget cuts',
-          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
-        ]
-      },
-      ' ',
-      'Yours Sincerely,',
-      ' ',
-      `${name}`,
-  
-    ]
-    
-  }
-  let letter1 = {
-    content: [
-      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
-      ' ',
-      {text:'Uphall Station Village Hall', alignment:"right" },
-      {text:'14 Marrfield Terrace',alignment:"right" },
-      {text:'Uphall Station',alignment:"right" },
-      {text:'EH54 5PY',alignment:"right" },
-      {text:'West Lothian',alignment:"right" },
-      {text:'United Kingdom',alignment:"right" },
-      ' ',
-      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
-      {text:'Contact Number: ',alignment:"right" },
-      {text:`Dear Councillor ${councName},`},
-      ' ',
-      `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
-      `,
-      ' ',
-      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
-      ' ',
-      ' ',
-      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
-      ' ',
-      {
-        ul: [
-          'What progress has been made in developing budget cut plans',
-          'Which Community Centres are at risk of closure',
-          'What information has the Council used to target budget cuts',
-          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
-        ]
-      },
-      ' ',
-      'Yours Sincerely,',
-      ' ',
-      `${name}`,
-  
-    ]
-    
-  }
-  let letter2 = {
-    content: [
-      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
-      ' ',
-      {text:'Uphall Station Village Hall', alignment:"right" },
-      {text:'14 Marrfield Terrace',alignment:"right" },
-      {text:'Uphall Station',alignment:"right" },
-      {text:'EH54 5PY',alignment:"right" },
-      {text:'West Lothian',alignment:"right" },
-      {text:'United Kingdom',alignment:"right" },
-      ' ',
-      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
-      {text:'Contact Number: ',alignment:"right" },
-      {text:`Dear Councillor ${councName},`},
-      ' ',
-      `As you will no doubt be aware our community centre in ${hall} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
-      `,
-      ' ',
-      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
-      ' ',
-      ' ',
-      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
-      ' ',
-      {
-        ul: [
-          'What progress has been made in developing budget cut plans',
-          'Which Community Centres are at risk of closure',
-          'What information has the Council used to target budget cuts',
-          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
-        ]
-      },
-      ' ',
-      'Yours Sincerely,',
-      ' ',
-      `${name}`,
-  
-    ]
-    
-  }
-  let letter3 = {
-    content: [
-      {text: 'Community Centre Budget Cuts 2025/26 to 2027/28', fontSize: 15, alignment:"centre" },
-      ' ',
-      {text:'Uphall Station Village Hall', alignment:"right" },
-      {text:'14 Marrfield Terrace',alignment:"right" },
-      {text:'Uphall Station',alignment:"right" },
-      {text:'EH54 5PY',alignment:"right" },
-      {text:'West Lothian',alignment:"right" },
-      {text:'United Kingdom',alignment:"right" },
-      ' ',
-      {text:new Date().toLocaleDateString('en-GB'),alignment:"right" },
-      {text:'Contact Number: ',alignment:"right" },
-      {text:`Dear Councillor ${councName},`},
-      ' ',
-      `As you will no doubt be aware our community centre in ${hall[0]} plays a vital role by providing a facility allowing a variety of groups and users to come together to meet the Council's objectives in this service area i.e. unders 5s, young people, the elderly and much more. Our centre offers a lifeline to many in our community.
-      `,
-      ' ',
-      `${pronoun} writing to you regarding West Lothian Council's decision in February 2023 to cut the Community Centre's budget by £1m over the next 3 years from 2025/26.`,
-      ' ',
-      ' ',
-      `As it has been eight months since this decision, and because the recently formed Steering Group have been left disappointed in the lack of forthcoming information from the Council which they could then relay to Management Committees ${pronounA} would like to ask the following questions:`,
-      ' ',
-      {
-        ul: [
-          'What progress has been made in developing budget cut plans',
-          'Which Community Centres are at risk of closure',
-          'What information has the Council used to target budget cuts',
-          'What consultation plans are proposed to advise the Community Centres Management Committees on these budget cuts and the intention to close them',
-        ]
-      },
-      ' ',
-      'Yours Sincerely,',
-      ' ',
-      `${name}`,
-  
-    ]
-    
-  }
+ let hall
 
-  let letters = [letter0, letter1, letter2, letter3]
+  
   
   function createLetter(){
     let feed = document.getElementById("feedback")
@@ -194,7 +35,7 @@ function App() {
 
 
   function council(doc,nameCounc, hallName){
-    councName = nameCounc
+    let councName = nameCounc
     for (let i=0; i<6; i++) doc.content[i+2] = hall[i]
     let telNo = document.getElementById("tel")
     if (telNo.value.length > 1) doc.content[10] = {text:`Contact Number: ${telNo.value}`,alignment:"right" }
